58ace63f96ddae113ccb146a7ce5e2943f9a8abe
Marta K <martakostyk@op.pl>
2018-08-30 14:34:49 +0200

    task 7 - create form

--- task7/src/main/java/martak/jjdd4/aftercourse/servlets/GetCreditParameters.java
+++ task7/src/main/java/martak/jjdd4/aftercourse/servlets/GetCreditParameters.java
@@ -1,6 +1,11 @@
 package martak.jjdd4.aftercourse.servlets;
 
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import martak.jjdd4.aftercourse.beans.ValidateCreditParameters;
+import martak.jjdd4.aftercourse.freemarker.TemplateProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -9,15 +14,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/parameters")
 public class GetCreditParameters extends HttpServlet {
 
+    private final Logger LOG = LoggerFactory.getLogger(GetCreditParameters.class);
+
     @Inject
-    ValidateCreditParameters validateCreditParameters;
+    private ValidateCreditParameters validateCreditParameters;
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Template template = templateProvider.getTemplate(getServletContext(), "get-credit-parameters.ftlh");
+        resp.setContentType("text/html;charset=UTF-8");
+
+        Map<String, Object> dataModel = new HashMap<>();
+
+        try {
+            template.process(dataModel, resp.getWriter());
+        } catch (TemplateException e) {
+            LOG.warn("Template not found");
+        }
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.getWriter().println("super!");
     }
 }
