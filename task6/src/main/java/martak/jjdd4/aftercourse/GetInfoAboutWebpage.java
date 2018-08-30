6cc72240e3245df5c6ecd611e35a0dff840016a3
Marta K <martakostyk@op.pl>
2018-08-30 13:05:26 +0200

    task 6 - resolve some bugs

--- task6/src/main/java/martak/jjdd4/aftercourse/GetInfoAboutWebpage.java
+++ task6/src/main/java/martak/jjdd4/aftercourse/GetInfoAboutWebpage.java
@@ -1,15 +1,17 @@
 package martak.jjdd4.aftercourse;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpOptions;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class GetInfoAboutWebpage {
@@ -20,50 +22,88 @@ public class GetInfoAboutWebpage {
 
     private URL url = null;
 
+
     private void getUrl() {
-        System.out.println("Hello! Please enter the webpage address: ");
 
         while (url == null) {
             try {
                 url = new URL(scanner.next());
             } catch (MalformedURLException e) {
                 System.out.println("Given address is incorrect. Please enter correct address: ");
-                LOG.info("Given address is incorrect");
+                LOG.info("Incorrect address has been given. Error message: " + e.getMessage());
             }
         }
     }
 
-    private void getInfoAboutWebsite() {
+    private boolean doesWebsiteExists() {
 
         try {
+            HttpURLConnection.setFollowRedirects(false);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            if (doesWebsiteExist(connection)) {
+            connection.setRequestMethod("HEAD");
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+            return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
 
-                connection.setRequestMethod("HEAD");
-                System.out.println("content length: " + connection.getContentLengthLong());
-                connection.setRequestMethod("OPTIONS");
-                System.out.println("allowed methods: " + connection.getHeaderField("Allow"));
-
-            } else {
-                System.out.println("Given webpage does not exist");
-                LOG.info("Given webpage does not exist");
-
-            }
-
-        } catch (IOException e) {
-            LOG.error("There has been error while trying to connect to given url address");
-            e.getStackTrace();
+        } catch (Exception e) {
+            System.out.println("Could not find given address. Website might not exist.");
+            LOG.info("Error while trying to connect with given address, error message: " + e.getMessage());
         }
+
+        return false;
     }
 
-    private boolean doesWebsiteExist(HttpURLConnection connection) throws IOException {
-        return connection.getResponseCode() == HttpURLConnection.HTTP_OK;
+    private String getContentLength() {
+
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpHead request = new HttpHead(String.valueOf(url));
+            HttpResponse response = client.execute(request);
+
+            if (response.containsHeader("content-length")) {
+                return response.getFirstHeader("content-length").getValue();
+            }
+
+        } catch (Exception e) {
+            LOG.info("Error while trying to get content length, error message: " + e.getMessage());
+        }
+
+        return "not found";
+    }
+
+    private String getHttpMethods() {
+
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpOptions request = new HttpOptions(String.valueOf(url));
+            HttpResponse response = client.execute(request);
+
+            if(response.containsHeader("allow")) {
+                return response.getFirstHeader("allow").getValue();
+            } else if (response.containsHeader("access-control-allow-methods")) {
+                return response.getFirstHeader("access-control-allow-methods").getValue();
+            }
+
+        } catch (Exception e) {
+            LOG.info("Error while trying to get allowed http methods, error message: " + e.getMessage());
+        }
+
+        return "not found";
+
     }
 
     public static void main(String[] args) {
 
         GetInfoAboutWebpage getInfoAboutWebpage = new GetInfoAboutWebpage();
+
+        System.out.println("Hello! Please enter the webpage address: ");
+
         getInfoAboutWebpage.getUrl();
-        getInfoAboutWebpage.getInfoAboutWebsite();
+
+        if (getInfoAboutWebpage.doesWebsiteExists()) {
+            System.out.println("Content length: " + getInfoAboutWebpage.getContentLength());
+            System.out.println("Allowed http methods: " + getInfoAboutWebpage.getHttpMethods());
+        } else {
+            System.out.println("Could not find given address. Website might not exist.");
+        }
     }
 }
